e79525adf29b9f6883168e2a72b1f487281de3ef
Huy Nguyen <huuhuy5208@gmail.com>
2022-09-29 13:42:41 +0700

    Add more 3

--- TestProject/src/Dance.java
+++ TestProject/src/Dance.java
@@ -4,7 +4,7 @@ public class Dance {
 	public static void main(String[] args) {
 		System.out.println("Watch those chickens dance!");
 		System.out.println("Watch those chickens dance1!");
-
+		System.out.println("Watch those chickens dance2!");
 	}
 
 }
